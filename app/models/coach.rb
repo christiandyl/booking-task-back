d3915c3066b518308a2795737274d077f2e2ebc1
Christian Dyl <christian@adminix.io>
2024-04-03 17:12:24 +0300

    the task is ready

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -17,17 +17,20 @@ class Coach < ApplicationRecord
       :available_until,
     ) => slot_struct
 
-    date = Date.parse(date_str)
-
-    local_time_start = ActiveSupport::TimeZone.new("UTC").parse("0:00 AM").in_time_zone(timezone)
+    # date = Date.parse(date_str)
+    start_date = ActiveSupport::TimeZone.new(timezone).parse(date_str).in_time_zone("UTC")
+    stop_date = (ActiveSupport::TimeZone.new(timezone).parse(date_str) + 1.day).in_time_zone("UTC")
+    # local_time_start = ActiveSupport::TimeZone.new("UTC").parse("0:00 AM").in_time_zone(timezone)
     # local_time_start = ActiveSupport::TimeZone.new(timezone).parse("0:00AM").in_time_zone("UTC")
     # local_time_stop = ActiveSupport::TimeZone.new(timezone).parse("24:00PM").in_time_zone("UTC")
 
+    # ActiveSupport::TimeZone.new(timezone).parse(date_str).in_time_zone("UTC").strftime('%A')
     filtered_slots = slots.includes(:coach).where(
-      "day_of_week = ? AND available_at >= ?",
-      Slot.day_of_weeks[date.strftime("%A").downcase],
-      local_time_start,
-      # local_time_stop,
+      "(day_of_week = ? AND available_at >= ?) OR (day_of_week = ? AND available_at <= ?)",
+      Slot.day_of_weeks[start_date.strftime('%A').downcase],
+      start_date.to_time,
+      Slot.day_of_weeks[stop_date.strftime('%A').downcase],
+      stop_date.to_time,
     )
 
     booked_slots = reserved_slots.where("reserved_at > ?", Date.today)
@@ -43,8 +46,8 @@ class Coach < ApplicationRecord
         slot.coach,
         slot.timezone,
         slot.day_of_week,
-        slot.available_at.in_time_zone(timezone).strftime('%Y-%m-%dT %H:%M:%S'),
-        slot.available_until.in_time_zone(timezone).strftime('%Y-%m-%dT %H:%M:%S'),
+        (slot.available_at.in_time_zone(timezone) + 1.hour).strftime('%Y-%m-%dT %H:%M:%S'),
+        (slot.available_until.in_time_zone(timezone) + 1.hour).strftime('%Y-%m-%dT %H:%M:%S'),
       )
     end
   end
